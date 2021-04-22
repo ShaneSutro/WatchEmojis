94fe16271c345afaed9abe15e2edd816ff34ee7e
Shane Sutro <shanesutro@gmail.com>
2021-04-22 16:19:18 -0600

    Debug

--- database/model.js
+++ database/model.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Line = require('./db')
 
 const connect = () => {
-    return mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/27017')
+    console.log(process.env.MONGODB_URI)
+    return mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017')
 }
 
 const models = { Line }
